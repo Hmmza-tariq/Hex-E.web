d1a8e0bc9114586f17a0538ae91921c3fd6bd7fe
Hmmza <111647708+Hmmza-tariq@users.noreply.github.com>
2023-02-11 00:27:12 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
 // We can define here the behavior for the elements on the website...
 
 // URL for POST requests
-const dalleEndpoint = 'https://api.openai.com/v1/images/generations';
+const dalleEndpoint = "https://api.openai.com/v1/images/generations";
 
 // Fetch DOM elements
-const reqButton = document.getElementById('button-request');
-const reqStatus = document.getElementById('request-status');
+const reqButton = document.getElementById("button-request");
+const reqStatus = document.getElementById("request-status");
 
 // Attach click behavior to the button
 reqButton.onclick = function () {
-
-  reqButton.disabled = true; 
+  reqButton.disabled = true;
 
   // Give some feedback to user
   reqStatus.innerHTML = "Request started...";
 
   // Fetch image request data
-  const key = document.getElementById('api-key').value;
-  const prompt = document.getElementById('text-prompt').value;
-  const count = Number(document.getElementById('image-count').value);
-  const radios = document.getElementsByName('image-size');
+  const key = document.getElementById("api-key").value;
+  const prompt = document.getElementById("text-prompt").value;
+  const count = Number(document.getElementById("image-count").value);
+  const radios = document.getElementsByName("image-size");
   let size;
   for (let i = 0; i < radios.length; i++) {
     if (radios[i].checked) {
@@ -62,72 +61,85 @@ reqButton.onclick = function () {
     prompt: prompt,
     n: count,
     size: size + "x" + size,
-    response_format: 'url',
+    response_format: "url",
   };
 
   // Form the data for a POST request:
   const reqParams = {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${key}`,
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${key}`,
     },
-    body: JSON.stringify(reqBody)
-  }
+    body: JSON.stringify(reqBody),
+  };
 
   // Use the Fetch API to do an async POST request to OpenAI:
   // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API
   fetch(dalleEndpoint, reqParams)
-    .then(res => res.json())
-    .then(json => addImages(json, prompt))
-    .catch(error => {
-        reqStatus.innerHTML = error;
-        reqButton.disabled = false;
+    .then((res) => res.json())
+    .then((json) => addImages(json, prompt))
+    .catch((error) => {
+      reqStatus.innerHTML = error;
+      reqButton.disabled = false;
     });
-}
-
-
+};
 
 /**
- * Adds images on the page 
+ * Adds images on the page
  * @param {Obj} jsonData The Dall-E API response
  * @param {String} prompt The original prompt that generated the images
  */
 function addImages(jsonData, prompt) {
-    
   // console.log(jsonData);
   reqButton.disabled = false;
 
   // Handle a possible error response from the API
-  if (jsonData.error)
-  {
-    reqStatus.innerHTML = 'ERROR: ' + jsonData.error.message;
+  if (jsonData.error) {
+    reqStatus.innerHTML = "ERROR: " + jsonData.error.message;
     return;
   }
-  
-  // Parse the response object, deserialize the image data, 
+
+  // Parse the response object, deserialize the image data,
   // and attach new images to the page.
-  const container = document.getElementById('image-container');
+  const container = document.getElementById("image-container");
   for (let i = 0; i < jsonData.data.length; i++) {
     let imgData = jsonData.data[i];
-    let img = document.createElement('img');
+    let img = document.createElement("img");
     img.src = imgData.url;
     img.alt = prompt;
     container.prepend(img);
   }
 
-  reqStatus.innerHTML = jsonData.data.length +' images received for "' + prompt + '"';
+  reqStatus.innerHTML =
+    jsonData.data.length + ' images received for "' + prompt + '"';
 }
 
-document.getElementById("download-button").addEventListener("click", function() {
-  var image = document.getElementById("image-container").firstChild;
-  var link = document.createElement("a");
-  link.download = "image.png";
-  link.href = image.src;
-  if (typeof link.download === 'undefined') {
-    window.open(image.src);
-  } else {
-    link.click();
-  }
-});
+document
+  .getElementById("download-button")
+  .addEventListener("click", function () {
+    var image = document.getElementById("image-container").firstChild;
+    var link = document.createElement("a");
+    link.download = "image.png";
+    link.href = image.src;
+    if (typeof link.download === "undefined") {
+      window.open(image.src);
+    } else {
+      link.click();
+    }
+    document.getElementById("minimize-button").style.display = "block";
+    document.getElementById("download-button").style.display = "none";
+  });
 
+document
+  .getElementById("minimize-button")
+  .addEventListener("click", function () {
+    // Minimize the image
+    document.getElementById("image-container").firstChild.style.display =
+      "none";
+
+    // Hide the minimize button
+    document.getElementById("minimize-button").style.display = "none";
+    document.getElementById("download-button").style.display = "block";
+
+  });
