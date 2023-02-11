17352562b334fc4c27c447ae19a8702a302344f5
Hmmza <111647708+Hmmza-tariq@users.noreply.github.com>
2023-02-11 14:41:32 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,49 @@
-// We can define here the behavior for the elements on the website...
+const listeningUI = `<svg width="25" height="25" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+<path d="M15.737 15.504C17.894 14.04 19.5 11.782 19.5 9a7.5 7.5 0 0 0-15 0v9.683c0 2.075 1.675 3.817 3.75 3.817s3.133-1.216 3.623-1.875c.692-.929 1.955-3.823 3.864-5.12Z"></path>
+<path d="M7.5 14.25V8.625C7.5 6.356 9.525 4.5 12 4.5s4.5 1.856 4.5 4.125"></path>
+<path d="M7.5 11.204c1.172-.844 3.742-.703 3.742-.703 1.218 0 1.93 1.379 1.218 2.372 0 0-1.728 1.987-1.962 2.878"></path>
+</svg>`;
+const micUI = `<svg width="25" height="25" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+<path d="M9 21h6"></path>
+<path d="M18 9.75v1.5c0 3.3-2.7 6-6 6s-6-2.7-6-6v-1.5"></path>
+<path d="M12 17.25V21"></path>
+<path d="M12 3a2.985 2.985 0 0 0-3 3v5.203c0 1.65 1.36 3.047 3 3.047s3-1.36 3-3.047V6c0-1.687-1.313-3-3-3Z"></path>
+</svg>`;
+var textPrompt = document.getElementById("text-prompt");
+var microphoneButton = document.getElementById("microphone-button");
+var recognition = new webkitSpeechRecognition() || SpeechRecognition;
+var listening = false;// We can define here the behavior for the elements on the website...
 
+
+microphoneButton.addEventListener("click", function () {
+  if (listening) {
+    microphoneButton.innerHTML = micUI;
+    recognition.stop();
+    listening = false;
+  } else {
+    microphoneButton.innerHTML = listeningUI;
+    var speech = true;
+    window.SpeechRecognition = window.webkitSpeechRecognition;
+
+    const recognition = new SpeechRecognition();
+    recognition.interimResults = true;
+
+    recognition.addEventListener("result", (e) => {
+      const transcript = Array.from(e.results)
+        .map((result) => result[0])
+        .map((result) => result.transcript)
+        .join("");
+
+      textPrompt.value = transcript;
+      console.log(transcript);
+    });
+
+    if (speech == true) {
+      recognition.start();
+    }
+    listening = true;
+  }
+});
 // URL for POST requests
 const dalleEndpoint = "https://api.openai.com/v1/images/generations";
 
